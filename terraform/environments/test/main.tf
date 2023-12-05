01d4b4ba0a8b3cbb250d73f10fbef08b3105c616
PhucTD <trandaiphuc.1501@gmail.com>
2023-12-05 21:25:28 +0700

    Update IaC

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
-  # tenant_id       = var.tenant_id
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  skip_provider_registration = true
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  # skip_provider_registration = true
   features {}
 }
 terraform {
