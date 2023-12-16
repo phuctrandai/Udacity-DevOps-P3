dbdda25833b6f90a58a35d6172d198b5a3ffcda7
PhucTD <trandaiphuc.1501@gmail.com>
2023-12-16 09:28:27 +0700

    Update IaC

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -12,15 +12,18 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                            = "${var.application_type}-LinuxVirtualMachine"
-  location                        = var.location
-  resource_group_name             = var.resource_group
-  size                            = "Standard_DS2_v2"
-  admin_username                  = "adminuser"
-  admin_password                  = "Password11-+"
+  name                = "${var.application_type}-LinuxVirtualMachine"
+  location            = var.location
+  resource_group_name = var.resource_group
+  size                = "Standard_DS2_v2"
+  admin_username      = "adminuser"
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2IZ08M2Nregsr1EPMxRE7qGs4ih589lJJD7Rz0FhW+OlBbymKW1M0v2Ld3qQiXB9t/j06/DLX9DxP0k1SdV2uOID4fFhlEx7O9JYgeeUpD59wvlfGSdckCeNEpiaxWKWesww12tYASUTTrzXM13hbqHt/l1DNgYIfhwBrJXhftGx5G9KJTaLmPHcdK/N82u4L19sCApUG4KoCLdrbAwvG0Tugvj1Rtw6Y17oXruRLZa4iykueUBjhma/tTeYufvQPaRKisl8FLKLH6gQDoOxV9nkhdyDetJN7szYh0PsfFXEGY8wRmDkiZB6vev03zoEN3nVaU0d/hGgh5DUd84cAfSfdNEGz7UucC1mkShTdLs+01mrKZrCNvUApTJduCHEnmhnnxLzfOZBfBcvpL66ZhvUmfBXjOGbY4bqdfjLRNP6bbiJmsHgWI8Pgw3oyvRnshqwV1keT5tStchjbUVhMq9r+cQokRU0B+6pQh0HI3rJScecIysYS3ucZeUGM13ARzVKvQ8c0BT5KBhOopoCGSjCuF8kuKWerWuas9dq5bJsgbJOQQRfFcSY0T40EEP+0P3w1xKr91OzOuw4yk4ukTyquYxFpjHx+txE5U+TV/otCZgehG4nEwkOmGSWd5ja/HGfZl5UhMIgVNuExySilwfDNtiYuO6i41iVa3hfbzw=="
+  }
   disable_password_authentication = false
   network_interface_ids           = [azurerm_network_interface.test.id]
-  
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
